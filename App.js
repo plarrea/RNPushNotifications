e14d7b127a96c8e20687ee42e04aae398ff74f28
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-13 14:02:51 -0500

    example using expo push service

--- App.js
+++ App.js
@@ -82,11 +82,29 @@ export default function App() {
     });
   };
 
+  const sendPushNotificationHandler = () => {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: '', // replace with actual token
+        title: 'Test - sent from a device',
+        body: 'This is a test',
+      }),
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Button
         title="Schedule Notification"
         onPress={scheduleNotificationHandler}
+      />
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationHandler}
       />
       <StatusBar style="auto" />
     </View>
