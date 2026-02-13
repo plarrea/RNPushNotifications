c3910e5cb9b2ede7b5667ddf26fee85139db8f1f
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-13 13:38:16 -0500

    local notification scheduler examples

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
+import * as Notifications from 'expo-notifications';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowBanner: true,
+      shouldShowList: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log('Notification Received', notification);
+        const userName = notification.request.content.data.userName;
+        console.log('Notification Received, userName:', userName);
+      },
+    );
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        const userName = response.notification.request.content.data.userName;
+        console.log('Notification Response Received, userName:', userName);
+      },
+    );
+
+    return () => {
+      subscription.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  const scheduleNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My First Local Notification',
+        body: 'This is the body of the notification',
+        data: {
+          userName: 'Pablo',
+        },
+        trigger: {
+          seconds: 5,
+        },
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
